f213a7e0befc6d2851366ad415e7d66076e95877
Chris M <chmccue@gmail.com>
2018-08-13 01:28:42 -0700

    extent report addition in progress.

--- src/test/java/sharedResources/supportMethods/FileRead.java
+++ src/test/java/sharedResources/supportMethods/FileRead.java
@@ -31,6 +31,11 @@ public class FileRead {
 		
 		return properties;
 	}
-	
+
+/*	public String getReportConfigPath(){
+		String reportConfigPath = properties.getProperty("reportConfigPath");
+		if(reportConfigPath!= null) return reportConfigPath;
+		else throw new RuntimeException("Report Config Path not specified in the Configuration.properties file for the Key:reportConfigPath");
+	}*/
 	
 }
--- src/test/java/sharedResources/testRunner/TestRunner.java
+++ src/test/java/sharedResources/testRunner/TestRunner.java
@@ -11,15 +11,21 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = { "src/test/resources/featureFiles" },
-		glue = {"sharedResources/webDriver", "stepDefinition", "sharedResources/testRunner"},
+		glue = {"sharedResources/webDriver", "stepDefinition", "sharedResources/testRunner", "com.vimalselvam.cucumber.stepdefinitions"},
 		tags = { "~@ignore"},
 		monochrome = true,
 		plugin = {"pretty", "html:target/cucumber",
-				"json:target/cucumber.json"
+				"json:target/cucumber.json",
+				"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"
+				//"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"
 				//"com.cucumber.listener.ExtentCucumberFormatter:target/report.html"
 		})
 public class TestRunner {
-	
+
 	public static Map<String, String> config;
 	public static Scenario scenario;
+
+
 }
+
+
